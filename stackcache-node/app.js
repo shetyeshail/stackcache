d0a52f85908f13b509092840eafacbd98ea48735
Waqas l <Waqas l>
2016-08-06 22:02:04 -0400

    titties

--- stackcache-node/app.js
+++ stackcache-node/app.js
@@ -1,12 +1,9 @@
-var request = require("request");
+var request = require("superagent")
 
-var url = "https://api.stackexchange.com/2.2/tags/java/faq?key=U4DMV*8nvpm3EOpvf69Rxw((&site=stackoverflow&filter=default";
-request({
-	url: url,
-	json: true
-}, function(err, response, body){
-	if (!err && response.statusCode === 200) {
-		var jsons = JSON.stringify(body)
-        console.log(jsons); // Print the json response
-    }
-})
+request
+	.get("https://api.stackexchange.com/2.2/tags/java/faq?site=stackoverflow")
+	.end(function(err, res){
+		res.body.items.forEach(function(item){
+			console.log(item.link);
+		})
+	})
